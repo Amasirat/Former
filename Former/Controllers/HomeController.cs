7516582eda92e2a1474a568bc54159b6854e1bf9
amir.basirat.2004@gmail.com <amir.basirat.2004@gmail.com>
2025-11-27 14:55:29 +0330

    added Tailwindcss to project

--- Former/Controllers/HomeController.cs
+++ Former/Controllers/HomeController.cs
@@ -10,9 +10,4 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 }
--- Former/Program.cs
+++ Former/Program.cs
@@ -75,9 +75,9 @@ public static class Program
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
+            app.UseHttpsRedirection();
         }
 
-        app.UseHttpsRedirection();
         app.UseRouting();
 
         app.UseAuthorization();
